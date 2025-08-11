96892c1b6409e5a8a630197278e3a5712106a80c
dongyeon1031 <ehddus0087@naver.com>
2025-08-11 14:44:51 +0900

    chore: requirments.txt 추가, model llama로 수정, model.py에 외부 코드 허용 설정 및 기타 설정 추가

--- inference/model.py
+++ inference/model.py
@@ -2,17 +2,19 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
 def load_model_and_tokenizer(model_name: str):
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, token=True, trust_remote_code=True)
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
         device_map="auto",
-        load_in_4bit=True,
-        torch_dtype=torch.float16
+        torch_dtype=torch.float16,
+        token=True,
+        trust_remote_code=True,
     )
     pipe = pipeline(
         "text-generation",
         model=model,
         tokenizer=tokenizer,
-        device_map="auto"
+        device_map="auto",
+        pad_token_id=tokenizer.eos_token_id
     )
     return pipe
