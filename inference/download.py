97b087d90bbf7a5242bc7d0ec23426b0793d0040
dongyeon1031 <ehddus0087@naver.com>
2025-08-25 14:51:13 +0900

    model: 모델 다운로드 옵션 수정

--- inference/download.py
+++ inference/download.py
@@ -8,5 +8,9 @@ def download_model():
             repo_id="LGAI-EXAONE/EXAONE-4.0-32B-GGUF",
             local_dir=model_dir,
             local_dir_use_symlinks=False,  # 하드카피로 저장
-            allow_patterns=["*Q6_K.gguf"]  # Q6_K 모델만 다운로드
+            allow_patterns=[
+                "*Q6_K.gguf",
+                "chat_template.jinja",
+                "assets/*"
+            ]  # Q6_K 모델, chat_template.jinja, assets 디렉토리 전체 다운로드
         )
