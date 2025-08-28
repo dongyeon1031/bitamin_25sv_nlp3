516449d59872f11c016f24de8eb8b7a0d670ab99
dongyeon1031 <ehddus0087@naver.com>
2025-08-28 11:52:28 +0900

    ci: shell로 modelfile 구성 및 모델 ollama로 서빙

--- install.sh
+++ install.sh
@@ -4,20 +4,35 @@
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# llama-cpp-python 설치 제거
+# llama-cpp-python 설치
 # CMAKE_ARGS="-DLLAMA_CUBLAS=OFF -DGGML_CUDA=ON" \
 # pip install llama-cpp-python[cuda12]==0.3.16 --no-cache-dir --force-reinstall --upgrade
 
 # ollama 설치 (리눅스 전용)
+sudo apt update
+sudo apt install -y pciutils lshw
+
 echo "[*] Installing Ollama..."
 curl -fsSL https://ollama.com/install.sh | sh
 
 # ollama 서버 실행 (백그라운드)
 echo "[*] Starting Ollama daemon..."
-ollama serve &
+nohup ollama serve > ollama.log 2>&1 &
 
-# 모델 다운로드 (예: DavidAU의 Qwen3-128k 30B 모델)
-echo "[*] Pulling model..."
-ollama pull davidau/qwen3-128k-30b-a3b-neo-max-ultra:Q6_K
+# 모델 디렉토리 생성 및 다운로드
+echo "[*] Downloading EXAONE model..."
+mkdir -p models
+wget https://huggingface.co/LGAI-EXAONE/EXAONE-4.0-32B-GGUF/resolve/main/EXAONE-4.0-32B-Q6_K.gguf \
+     -O ./models/EXAONE-4.0-32B-Q6_K.gguf
 
-echo "[*] All done !"
+# Modelfile 생성
+echo "[*] Creating Modelfile..."
+cat <<EOF > Modelfile
+FROM ./models/EXAONE-4.0-32B-Q6_K.gguf
+EOF
+
+# Ollama 모델 등록
+echo "[*] Registering model to Ollama..."
+ollama create exaone-custom -f Modelfile
+
+echo "[*] 끝~~"
